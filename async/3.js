9c81d31d23746f67cdf1d382ec6f6cdfff727e72
楼教主 <cik520@qq.com>
2016-07-12 21:48:37 +0800

    async 并发例子

--- async/3.js
+++ async/3.js
@@ -1,5 +1,5 @@
 function sleep(t) {
-  return new Promise(resolve => setTimeout(resolve, t, +new Date))
+  return new Promise(resolve => setTimeout( _ => { resolve(+new Date) }, t))
 }
 
 async function run() {
@@ -7,21 +7,26 @@ async function run() {
   let a = await sleep(100)
   let b = await sleep(200)
 
-  // 并发
+  // 并发1
   let c = await Promise.all([sleep(100), sleep(200), sleep(300)])
 
   // 并发2
+  let d = await Promise.all([100, 200, 300].map(t => sleep(t)))
+
+  // 并发3
   let list = [sleep(100), sleep(200), sleep(300)]
-  let d = []
+  let e = []
   for (let fn of list) {
-    d.push(await fn);
+    e.push(await fn)
   }
 
-  console.log('',
+  console.log(
+    '',
     'a:', a, '\n',
     'b:', b, '\n',
-    c, '\n',
-    d, '\n'
+    'c:', c, '\n',
+    'd:', d, '\n',
+    'e:', e, '\n'
   )
 }
 
