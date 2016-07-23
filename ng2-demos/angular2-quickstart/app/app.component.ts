41bece2dcaa6c76c27878f7b75508b5bf5a1a13e
楼教主 <cik520@qq.com>
2016-07-23 23:49:00 +0800

    添加用例

---
+++ ng2-demos/angular2-quickstart/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>我的第一个 Angular 2 应用!</h1>'
+})
+export class AppComponent { }
--- ng2-demos/readme.md
+++ ng2-demos/readme.md
@@ -1,3 +1,3 @@
 ## Angular 2 学习笔记
 
-> <http://www.angular.cn/> 官网教程demos
+> <https://angular.cn/> 官网教程demos
